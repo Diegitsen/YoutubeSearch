d6ae057da6797805c09cb564560dd3407e4b2e85
diegitsen <diegitsen@gmail.com>
2020-02-09 17:47:21 -0500

    finished

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,7 +2,7 @@ import 'package:kiwi/kiwi.dart' as kiwi;
 import 'package:http/http.dart' as http;
 import 'package:ytsearch/data/network/youtube_data_source.dart';
 import 'package:ytsearch/data/repository/youtube_repository.dart';
-//import 'package:ytsearch/ui/detail/detail_bloc.dart';
+import 'package:ytsearch/ui/detail/detail_bloc.dart';
 import 'package:ytsearch/ui/search/search_bloc.dart';
 
 void initKiwi() {
@@ -10,5 +10,6 @@ void initKiwi() {
     ..registerInstance(http.Client())
     ..registerFactory((c) => YoutubeDataSource(c.resolve()))
     ..registerFactory((c) => YoutubeRepository(c.resolve()))
-    ..registerFactory((c) => SearchBloc(c.resolve()));
+    ..registerFactory((c) => SearchBloc(c.resolve()))
+    ..registerFactory((c) => DetailBloc(c.resolve()));
 }
